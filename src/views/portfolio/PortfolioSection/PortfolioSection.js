197c5971ee9f3d151901fd1bf671a79dd82e88a3
elliot richardson <ear78@aol.com>
2019-12-02 00:00:21 -0800

    added images and updated data for portfolios

--- src/views/portfolio/PortfolioSection/PortfolioSection.js
+++ src/views/portfolio/PortfolioSection/PortfolioSection.js
@@ -9,6 +9,16 @@ import beoplay1 from '../../../assets/img/beoplay1.png'
 import beoplay2 from '../../../assets/img/beoplay2.png'
 import beoplay3 from '../../../assets/img/beoplay3.png'
 import beoplay4 from '../../../assets/img/beoplay4.jpg'
+import rodesk1 from '../../../assets/img/rodesk1.jpg'
+import rodesk2 from '../../../assets/img/rodesk2.jpg'
+import rodesk3 from '../../../assets/img/rodesk3.jpg'
+import lyft1 from '../../../assets/img/lyft1.jpg'
+import lyft2 from '../../../assets/img/lyft2.jpg'
+import lyft3 from '../../../assets/img/lyft3.jpg'
+import optins1 from '../../../assets/img/optins1.png'
+import optins2 from '../../../assets/img/optins2.png'
+import emp1 from '../../../assets/img/emp1.png'
+import emp2 from '../../../assets/img/emp2.jpg'
 
 function portfolioSection(props) {
   let portfolioData = [
@@ -18,19 +28,19 @@ function portfolioSection(props) {
       images: [beoplay1, beoplay2, beoplay3, beoplay4],
     },
     {
-      title: 'Beoplay Website Clone',
-      description: 'HTML, CSS, Javascript, Jquery, Angular, Gulp, Sass, Node.js, SQL',
-      images: [beoplay1, beoplay2, beoplay3, beoplay4],
+      title: 'RODESK WEBSITE CLONE',
+      description: 'HTML, CSS, Javascript, Angular, Jquery, Sketch',
+      images: [rodesk1, rodesk2, rodesk3, beoplay4],
     },
     {
-      title: 'Beoplay Website Clone',
-      description: 'HTML, CSS, Javascript, Jquery, Angular, Gulp, Sass, Node.js, SQL',
-      images: [beoplay1, beoplay2, beoplay3, beoplay4],
+      title: 'LYFT WEBSITE CLONE',
+      description: 'HTML, CSS, Javascript, Jquery, React, Webpack, Node.js, SQL',
+      images: [lyft1, lyft2, lyft3, beoplay4],
     },
     {
-      title: 'Beoplay Website Clone',
-      description: 'HTML, CSS, Javascript, Jquery, Angular, Gulp, Sass, Node.js, SQL',
-      images: [beoplay1, beoplay2, beoplay3, beoplay4],
+      title: 'OPTIMAL INSURANCE, EMPOWERED EMPLOYMENT, SOCDETOX, WE YOGA',
+      description: 'Custom HTML, CSS, Squarespace CMS, Wordpress, Affinity Designer, Affinity Photo',
+      images: [optins1, optins2, emp1, emp2],
     },
   ]
 
