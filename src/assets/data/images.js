a00a104d1e8da8ef55b044c0a17db6121b02718a
elliot richardson <ear78@aol.com>
2019-12-17 22:42:13 -0800

    moved imported images to images.js and cleaned up PortfolioSection components

--- src/assets/data/images.js
+++ src/assets/data/images.js
@@ -8,6 +8,21 @@ import gulpImage from '../img/gulp-blk.png'
 import sassImage from '../img/sass-blu.png'
 import vueImage from '../img/vuejs-logo.png'
 
+import beoplay1 from '../img/beoplay1.png'
+import beoplay2 from '../img/beoplay2.png'
+import beoplay3 from '../img/beoplay3.png'
+import beoplay4 from '../img/beoplay4.jpg'
+import rodesk1 from '../img/rodesk1.jpg'
+import rodesk2 from '../img/rodesk2.jpg'
+import rodesk3 from '../img/rodesk3.jpg'
+import lyft1 from '../img/lyft1.jpg'
+import lyft2 from '../img/lyft2.jpg'
+import lyft3 from '../img/lyft3.jpg'
+import optins1 from '../img/optins1.png'
+import optins2 from '../img/optins2.png'
+import emp1 from '../img/emp1.png'
+import emp2 from '../img/emp2.jpg'
+
 export const skillLogos = [
   {logo: cssImage, altText: 'css logo'},
   {logo: htmlImage, altText: 'html logo'},
@@ -18,4 +33,27 @@ export const skillLogos = [
   {logo: gulpImage, altText: 'gulp logo'},
   {logo: sassImage, altText: 'sass logo'},
   {logo: vueImage, altText: 'vuejs logo'},
+]
+
+export const portfolioData = [
+  {
+    title: 'Beoplay Website Clone',
+    description: 'HTML, CSS, Javascript, Jquery, Angular, Gulp, Sass, Node.js, SQL',
+    images: [beoplay1, beoplay2, beoplay3, beoplay4],
+  },
+  {
+    title: 'RODESK WEBSITE CLONE',
+    description: 'HTML, CSS, Javascript, Angular, Jquery, Sketch',
+    images: [rodesk1, rodesk2, rodesk3, beoplay4],
+  },
+  {
+    title: 'LYFT WEBSITE CLONE',
+    description: 'HTML, CSS, Javascript, Jquery, React, Webpack, Node.js, SQL',
+    images: [lyft1, lyft2, lyft3, beoplay4],
+  },
+  {
+    title: 'OPTIMAL INSURANCE, EMPOWERED EMPLOYMENT, SOCDETOX, WE YOGA',
+    description: 'Custom HTML, CSS, Squarespace CMS, Wordpress, Affinity Designer, Affinity Photo',
+    images: [optins1, optins2, emp1, emp2],
+  },
 ]
--- src/views/portfolio/PortfolioSection/PortfolioSection.js
+++ src/views/portfolio/PortfolioSection/PortfolioSection.js
@@ -1,49 +1,12 @@
 import React from 'react'
 import styles from './PortfolioSection.module.css'
+import { portfolioData } from '../../../assets/data/images'
 
 import PortfolioDesc from './PortfolioDesc/PortfolioDesc'
 import PortfolioImage from './PortfolioImage/PortfolioImage'
 import SectionTitle from '../../../components/SectionTitle/SectionTitle'
 
-import beoplay1 from '../../../assets/img/beoplay1.png'
-import beoplay2 from '../../../assets/img/beoplay2.png'
-import beoplay3 from '../../../assets/img/beoplay3.png'
-import beoplay4 from '../../../assets/img/beoplay4.jpg'
-import rodesk1 from '../../../assets/img/rodesk1.jpg'
-import rodesk2 from '../../../assets/img/rodesk2.jpg'
-import rodesk3 from '../../../assets/img/rodesk3.jpg'
-import lyft1 from '../../../assets/img/lyft1.jpg'
-import lyft2 from '../../../assets/img/lyft2.jpg'
-import lyft3 from '../../../assets/img/lyft3.jpg'
-import optins1 from '../../../assets/img/optins1.png'
-import optins2 from '../../../assets/img/optins2.png'
-import emp1 from '../../../assets/img/emp1.png'
-import emp2 from '../../../assets/img/emp2.jpg'
-
 function portfolioSection(props) {
-  let portfolioData = [
-    {
-      title: 'Beoplay Website Clone',
-      description: 'HTML, CSS, Javascript, Jquery, Angular, Gulp, Sass, Node.js, SQL',
-      images: [beoplay1, beoplay2, beoplay3, beoplay4],
-    },
-    {
-      title: 'RODESK WEBSITE CLONE',
-      description: 'HTML, CSS, Javascript, Angular, Jquery, Sketch',
-      images: [rodesk1, rodesk2, rodesk3, beoplay4],
-    },
-    {
-      title: 'LYFT WEBSITE CLONE',
-      description: 'HTML, CSS, Javascript, Jquery, React, Webpack, Node.js, SQL',
-      images: [lyft1, lyft2, lyft3, beoplay4],
-    },
-    {
-      title: 'OPTIMAL INSURANCE, EMPOWERED EMPLOYMENT, SOCDETOX, WE YOGA',
-      description: 'Custom HTML, CSS, Squarespace CMS, Wordpress, Affinity Designer, Affinity Photo',
-      images: [optins1, optins2, emp1, emp2],
-    },
-  ]
-
   let project = portfolioData.map(d => {
     return <section className={styles.ProjectWrapper}>
             <SectionTitle marginBottom={10}>{ d.title }</SectionTitle>
